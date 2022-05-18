36a0e9734bf3ca33a0f2d060a9d9e9c80b317d00
Samuel Dela Cruz <samsamcoding@gmail.com>
2022-05-18 21:50:31 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <recruitment-survey></recruitment-survey>
+  <user-experiences></user-experiences>
+</template>
+
+<script>
+import RecruitmentSurvey from './components/survey/RecruitmentSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
+
+export default {
+  components: {
+    RecruitmentSurvey,
+    UserExperiences,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
